453b8667996f0d67a01b16321b1d38d12ce7953c
lemon-fresh-air <green.maksym.green@gmail.com>
2026-09-12 18:27:14 +0300

    Add modular source: src/js/app.js

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+function render(){
+  document.getElementById('balanceHeader').innerHTML=balanceHeader();
+  document.getElementById('tab-gas').innerHTML=gasPage();
+  document.getElementById('tab-petrol').innerHTML=petrolPage();
+  document.getElementById('tab-calc').innerHTML=calcPage();
+  document.getElementById('tab-history').innerHTML=historyPage();
+  document.getElementById('tab-stats').innerHTML=statsPage();
+  document.getElementById('tab-settings').innerHTML=settingsPage();
+  syncUiChrome();bindDynamicEvents();bindFormattedInputs();
+  bindOdoWidget();
+}
+
+initApp();
+
